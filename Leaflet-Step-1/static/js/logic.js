565184ba15053c92eb85e92db49f21a0b6cf9789
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-09-14 22:53:23 -0400

    adding circle markers for each earthquake

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,19 +7,53 @@ var url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geo
 var myMap = L.map("map", {
     // center of the United States
     center: [39.8, -98.6], 
-    zoom: 10
+    zoom: 5
 });
 
 // add tile layer 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 5,
+    maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(myMap);
 
-// d3.json(url, function(data) {
+d3.json(url, function(response) {
+ 
+    console.log(response);
 
-// })
+  
+    // L.geoJSON(response, {
+    //     style: function (feature) {
+    //         return {color: feature.properties.mag};
+    //     }
+    // }).bindPopup(function (layer) {
+    //     return layer.feature.properties.place;
+    // }).addTo(myMap);
+
+    // var geojsonMarkerOptions = {
+    //     radius: feature.properties.mag,
+    //     fillColor: "blue",
+    //     color: "red",
+    //     weight: 20,
+    //     opacity: 1,
+    //     fillOpacity: 0.8
+    // };
+    
+    L.geoJSON(response, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: feature.properties.mag*10,
+                color: feature.properties.mag,
+                // weight: 20,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        }
+    }).bindPopup(function (layer) {
+            return (layer.feature.properties.place);
+        }).addTo(myMap);
+    
+});
